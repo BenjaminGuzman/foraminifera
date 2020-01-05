1a5e97cf469b7598ae153ad278e59cd10e28cb60
Benjamín Guzmán <9benjaminguzman@gmail.com>
2020-01-05 04:25:32 +0000

    Updated README.md & generated segmented_cropped images

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# foraminifera
+# Foraminifera
+
 Foraminifera species identification using supervised machine learning
+
+### Directory structure
+
+- `img`: directory where all images should go. **DO NOT** upload this directory to GitHub please, use another service like OneDrive or Google Drive. In my case you can download the directory from _url to directory_. The contents of the directory are:
+  
+  - `species`: Original images dataset, sorted by foraminifera specie. Obtained from https://research.ece.ncsu.edu/aros/foram-identification/
+  
+  - `segmented`: Segmented images. Images with everything but the foraminifera.
+  
+  - `segmented_cropped`: Same as `segmented` directory but these images are cropped. **This is the training dataset**.
+
+#### Files
+
+- `segmentation.py`: Code used to generate the `species`, `segmented` and `segmented_cropped` directories.
+
+
--- segmentation.py
+++ segmentation.py
@@ -2,17 +2,148 @@ import os
 import sys
 import shutil
 
+import cv2
+import numpy as np
+
 import progressbar
 
 DATASET_DIR = "../foraminifera-dataset"
 IMG_DIR = "img"
 OUT_SEGMENTED_IMAGES_DIR = "img/segmented"
+OUT_SEGMENTED_CROPPED_IMAGES_DIR = "img/segmented_cropped"
 OUT_SPECIES_IMAGES_DIR = "img/species"
 
 
+def binarize_image(image):
+    image = cv2.GaussianBlur(image, (5, 5), 0)  # make smoother contours
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # so we can do binarization below
+                                                # empiric threshold
+    ret, image_thresh = cv2.threshold(gray_image, 30, 255, cv2.THRESH_BINARY)  # binarization
+    return image_thresh
+
+
+def get_contours(image_bin, original_image=None):
+    contours, hierarchy = cv2.findContours(image_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    image_with_contours = None
+    if original_image is not None:
+        image_with_contours = cv2.drawContours(original_image, contours, -1, (255, 0, 255))
+
+    return contours, image_with_contours
+
+
+def get_and_clean_roi(image_bin, original_image):
+    contours, _ = get_contours(image_bin)
+
+    contour_areas = [(contour, cv2.contourArea(contour)) for contour in contours]
+
+    # obtain the greatest contour in the image, possibly the contour of the foraminifera
+    max_contour = max(contour_areas, key=lambda contour_tuple: contour_tuple[1])
+    max_contour_contour, max_contour_area = max_contour
+
+    # create a new image with only the ROI
+    # this image will be the mask
+    image_shape = image_bin.shape[:2]  # width & height, 3rd position is the number of channels
+    black_image = np.zeros(image_shape, np.uint8)
+    image_mask = cv2.fillConvexPoly(black_image, max_contour_contour, (255, 255, 255))
+
+    # apply mask
+    image_roi = cv2.bitwise_and(original_image, original_image, mask=image_mask)
+
+    return image_roi
+
+
 def clear_images():
-    # TODO: clear images, remove small particles and standardize image size
-    pass
+    DEBUGGING = False
+    MAX_FILES = 10
+    i = 0
+
+    progress_bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength, redirect_stdout=True)
+
+    for specie_dir in os.listdir(OUT_SPECIES_IMAGES_DIR):
+
+        specie_dir_path = os.path.join(OUT_SPECIES_IMAGES_DIR, specie_dir)
+
+        specie_dir_out_path = os.path.join(OUT_SEGMENTED_IMAGES_DIR, specie_dir)
+        if not os.path.isdir(specie_dir_out_path):
+            os.mkdir(specie_dir_out_path)
+
+        for image_name in os.listdir(specie_dir_path):
+            image_path = os.path.join(specie_dir_path, image_name)
+
+            if not os.path.isfile(image_path):
+                continue
+
+            image = cv2.imread(image_path)
+
+            image_bin = binarize_image(image)
+
+            image_roi = get_and_clean_roi(image_bin, image)
+
+            if DEBUGGING:
+                cv2.imshow("Original", image)
+                cv2.imshow("Binarized", image_bin)
+                cv2.imshow("ROI", image_roi)
+                cv2.waitKey(0)
+
+                if i > MAX_FILES:
+                    return
+            else:
+                file_abs_path = os.path.join(specie_dir_out_path, image_name)
+                cv2.imwrite(file_abs_path, image_roi)
+                progress_bar.update(i)
+
+            i += 1
+
+    return 0
+
+
+def crop_roi_images():
+    progress_bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength, redirect_stdout=True)
+    i = 0
+
+    for specie_dir in os.listdir(OUT_SEGMENTED_IMAGES_DIR):
+
+        specie_dir_path = os.path.join(OUT_SEGMENTED_IMAGES_DIR, specie_dir)
+
+        specie_dir_out_path = os.path.join(OUT_SEGMENTED_CROPPED_IMAGES_DIR, specie_dir)
+        if not os.path.isdir(specie_dir_out_path):
+            os.mkdir(specie_dir_out_path)
+
+        for image_name in os.listdir(specie_dir_path):
+            image_path = os.path.join(specie_dir_path, image_name)
+
+            if not os.path.isfile(image_path):
+                continue
+
+            image = cv2.imread(image_path)
+            image_bin = binarize_image(image)
+
+            contours, _ = get_contours(image_bin)
+
+            # get the ROI, the foraminifera contour
+            max_contour = max(contours, key=cv2.contourArea)
+
+            min_point = [
+                min(max_contour, key=lambda contour: contour[0][0])[0][0],
+                min(max_contour, key=lambda contour: contour[0][1])[0][1]
+            ]
+            max_point = [
+                max(max_contour, key=lambda contour: contour[0][0])[0][0],
+                max(max_contour, key=lambda contour: contour[0][1])[0][1]
+            ]
+
+            # crop with a square the image
+            image_cropped = image[min_point[1]:max_point[1], min_point[0]:max_point[0]]
+
+            file_abs_path = os.path.join(specie_dir_out_path, image_name)
+
+            cv2.imwrite(file_abs_path, image_cropped)
+            progress_bar.update(i)
+
+            i += 1
+
+    return 0
 
 
 def check_dirs():
@@ -20,16 +151,13 @@ def check_dirs():
         sys.stderr.write("\"{}\" DOES NOT EXISTS, exiting...".format(DATASET_DIR))
         sys.exit(1)
 
-    for dir_path in [IMG_DIR, OUT_SEGMENTED_IMAGES_DIR, OUT_SPECIES_IMAGES_DIR]:
+    for dir_path in [IMG_DIR, OUT_SEGMENTED_IMAGES_DIR, OUT_SPECIES_IMAGES_DIR, OUT_SEGMENTED_CROPPED_IMAGES_DIR]:
         if not os.path.isdir(dir_path):
             print("Directory \"{}\" does not exists, creating directory...".format(dir_path))
             os.mkdir(dir_path)
 
 
 def move_images():
-    # first check all directories are ok
-    check_dirs()
-
     progress_bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength, redirect_stdout=True)
     species_count = {}
 
@@ -55,7 +183,8 @@ def move_images():
                         _, file_extension = os.path.splitext(file)
                         species_count[specie_name] += 1
                         curr_file_abs_path = os.path.join(sub_directory, file)
-                        new_file_abs_path = os.path.join(specie_directory_abs_path, "{}{}".format(species_count[specie_name], file_extension))
+                        new_file_abs_path = os.path.join(specie_directory_abs_path,
+                                                         "{}{}".format(species_count[specie_name], file_extension))
 
                         # print(curr_file_abs_path, new_file_abs_path)
                         # shutil.move(curr_file_abs_path, new_file_abs_path)
@@ -66,7 +195,10 @@ def move_images():
 
 
 def main():
-    move_images()
+    check_dirs()
+    # move_images()
+    # clear_images()
+    crop_roi_images()
 
 
 if __name__ == '__main__':
